9ea086a67774f4a92e0acc856d156dbe178994a6
tcalicoAIR <tcalico@air.org>
2017-10-03 15:03:34 -0400

    File 3 only keeps relevant variables.

--- Code/3. ComputeThetas.R
+++ Code/3. ComputeThetas.R
@@ -18,22 +18,41 @@ source(paste0(codeDir, "fRecastThetas.R"))
 # Step 1: Get student usage information.
 studentData <- fread(paste0(inputDir, studentDataFileName))
 
+# Keep only relevant variables
+if (grade == 4) {
+	varsToKeep <-		c("IDCNTRY", "TOTWGT", "JKZONE", "JKREP",
+									 paste0("ASMMAT", str_pad(1:numberOfPVs, 2, pad = "0")),
+									 paste0("ASMDAT", str_pad(1:numberOfPVs, 2, pad = "0")),
+									 paste0("ASMGEO", str_pad(1:numberOfPVs, 2, pad = "0")),
+									 paste0("ASMNUM", str_pad(1:numberOfPVs, 2, pad = "0")))
+} else if (grade == 8) {
+	varsToKeep <-		c("IDCNTRY", "TOTWGT", "JKZONE", "JKREP",
+										paste0("BSMMAT", str_pad(1:numberOfPVs, 2, pad = "0")),
+									 	paste0("BSMALG", str_pad(1:numberOfPVs, 2, pad = "0")),
+									 	paste0("BSMDAT", str_pad(1:numberOfPVs, 2, pad = "0")),
+										paste0("BSMGEO", str_pad(1:numberOfPVs, 2, pad = "0")),
+									 	paste0("BSMNUM", str_pad(1:numberOfPVs, 2, pad = "0")))
+}
+
+studentData <- studentData[, ..varsToKeep]
+
+
 # Step 2: Get scaling constants.
 scalingConstants <- fread(paste0(inputDir,scalingConstantsFile))
 
 # Step 3: Compute thetas.
 
 if (analysisMode == 'Domain') {
-  lapply(names(traitsByTopics), computeThetas)
+	lapply(names(traitsByTopics), computeThetas)
 } else if (analysisMode == 'General' & grade == '4') {
-  computeThetas('ASMMAT')
+	computeThetas('ASMMAT')
 } else if (analysisMode == 'General' & grade == '8') {
-  computeThetas('BSMMAT')
+	computeThetas('BSMMAT')
 }
 
 # Save it as .csv for sharing and as .Rdata to facilitate use in R.
 if (saveIntermediate == TRUE) {
-  write.csv(studentData,
-            paste0(outputDir, "studentData_", newYear, "_Grd", grade, "_", analysisMode, ".csv"),
-            row.names = FALSE)
+	write.csv(studentData,
+						paste0(outputDir, "studentData_", newYear, "_Grd", grade, "_", analysisMode, ".csv"),
+						row.names = FALSE)
 }
