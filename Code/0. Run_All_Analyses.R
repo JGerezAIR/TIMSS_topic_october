4f9ba1c3d8d0664b3e1f8308ff6aa79ea0ea9b58
tcalicoAIR <tcalico@air.org>
2017-10-04 16:48:11 -0400

    Corrected File 0. to correctly call each of the script files.

--- Code/0. Run_All_Analyses.R
+++ Code/0. Run_All_Analyses.R
@@ -1,10 +1,19 @@
 # Run all analyses.
 
-#runDir 		 <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+source(paste0(runDir, "1. Run_2011_Grd4_Domain.R"))
 
-source("1. Run_2011_Grd4_Domain.R")
-source("1. Run_2011_Grd4_General.R")
-source("1. Run_2015_Grd4_Domain.R")
-source("1. Run_2015_Grd4_General.R")
-source("1. Run_2015_Grd8_Domain.R")
-source("1. Run_2015_Grd8_General.R")
+runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+source(paste0(runDir, "1. Run_2011_Grd4_General.R"))
+
+runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+source(paste0(runDir, "1. Run_2015_Grd4_Domain.R"))
+
+runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+source(paste0(runDir, "1. Run_2015_Grd4_General.R"))
+
+runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+source(paste0(runDir, "1. Run_2015_Grd8_Domain.R"))
+
+runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+source(paste0(runDir, "1. Run_2015_Grd8_General.R"))
