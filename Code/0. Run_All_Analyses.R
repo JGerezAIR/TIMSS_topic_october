83ae2b7bf524888be17bad3233bcde91dfa651f4
Tiago Calico <tcalico@air.org>
2017-10-16 13:32:56 -0400

    Updated script that calls all 1. scripts to use adress on H drive.

--- Code/0. Run_All_Analyses.R
+++ Code/0. Run_All_Analyses.R
@@ -1,19 +1,19 @@
 # Run all analyses.
 
-runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "H:/5.2_secondary/TIMSS/TIMSS topic study/Production/Code/"
 source(paste0(runDir, "1. Run_2011_Grd4_Domain.R"))
 
-runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "H:/5.2_secondary/TIMSS/TIMSS topic study/Production/Code/"
 source(paste0(runDir, "1. Run_2011_Grd4_General.R"))
 
-runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "H:/5.2_secondary/TIMSS/TIMSS topic study/Production/Code/"
 source(paste0(runDir, "1. Run_2015_Grd4_Domain.R"))
 
-runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "H:/5.2_secondary/TIMSS/TIMSS topic study/Production/Code/"
 source(paste0(runDir, "1. Run_2015_Grd4_General.R"))
 
-runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "H:/5.2_secondary/TIMSS/TIMSS topic study/Production/Code/"
 source(paste0(runDir, "1. Run_2015_Grd8_Domain.R"))
 
-runDir <- "C:/Users/tcalico/Desktop/TIMSS_topic_october/Code/"
+runDir <- "H:/5.2_secondary/TIMSS/TIMSS topic study/Production/Code/"
 source(paste0(runDir, "1. Run_2015_Grd8_General.R"))
